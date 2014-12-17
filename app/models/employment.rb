3336280896f2f6b72dcbd0dc902f7f57d9c802c1
tophat8855 <hrfarley@gmail.com>
2014-12-17 11:37:42 -0800

    set up associations

--- app/models/employment.rb
+++ app/models/employment.rb
@@ -1,2 +1,4 @@
 class Employment < ActiveRecord::Base
+  belongs_to :organization
+  belongs_to :person
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,4 @@
 class Organization < ActiveRecord::Base
+  has_many :employments
+  has_many :people, through: :employment
 end
